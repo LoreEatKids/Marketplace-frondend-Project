5fbc85e8f92af5bb3e0462a4ea5fbce739ebe9ff
Lorenzo Galetto <88822812+LoreEatKids@users.noreply.github.com>
2022-12-17 16:34:29 +0100

    final version

--- components/scripts/item.js
+++ components/scripts/item.js
@@ -3,13 +3,22 @@ import { productShowCase as imgs } from "./config.js";
 const container = document.querySelector(".img-section");
 const color1El = document.querySelector("#color-pink");
 const color2El = document.querySelector("#color-black");
+const inputs = document.querySelectorAll("input[name='NikeSuitTaglia']");
+const inputsColor = document.querySelectorAll("input[name='NikeSuit']");
 
+const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
 
 function displayItemsLight(arr) {
     arr.forEach(el => {
         const html = `<img src="${el}" alt="Maglietta" id="product" draggable="false">` 
         container.insertAdjacentHTML("beforeend", html);
     });
+
+    inputsColor.forEach(input => {
+        if (input.checked) {
+            input.closest(".details-type-color-1").querySelector("img").classList.add("selected");
+        }
+    })
 }
 
 displayItemsLight(imgs.linksLight)
@@ -20,9 +29,7 @@ color1El.addEventListener("click", ()=> {
     document.querySelector(".color1-img").style.border = "1px solid #000";
     document.querySelector(".color2-img").style.border = "0";
 
-    products.forEach((product, x) => {
-        product.src = imgs.linksLight[x];
-    })
+    products.forEach((product, x) => { product.src = imgs.linksLight[x]; })
 })
 
 color2El.addEventListener("click", ()=> {
@@ -32,40 +39,27 @@ color2El.addEventListener("click", ()=> {
     document.querySelector(".color2-img").style.border = "1px solid #000";
 
 
-    products.forEach((product, x) => {
-        product.src = imgs.linksDark[x];
-    })
-
-    const inputs = document.querySelectorAll("input[name='NikeSuit']");
-
+    products.forEach((product, x) => { product.src = imgs.linksDark[x]; })
 })
 
-const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
-
 document.querySelector("#confirm-btn").addEventListener("click", ()=> {
-    const inputs = document.querySelectorAll("input[name='NikeSuitTaglia']");
-    const inputsColor = document.querySelectorAll("input[name='NikeSuit']");
-    let existing = localStorage.getItem("cart")
-
     for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].checked) {
+        if (!inputs[i].checked || !inputsColor[i].checked) return;
 
-            const value = inputs[i].value;
+        const value = inputs[i].value;
+        const valueName = value.split(":")[0];
+        const valueTaglia = value.split(":")[1];
 
-            const valueName = value.split(":")[0];
-            const valueTaglia = value.split(":")[1];
+        inputsColor.forEach(color => {
+            if(!color.checked) return;
 
-            inputsColor.forEach(color => {
-                if (color.checked) {
-                    const valueColor = color.value.split(":")[2];
+            const valueColor = color.value.split(":")[2];
                     
-                    cartItems.push([valueName, valueTaglia, valueColor])
-                    window.localStorage.setItem("cart", JSON.stringify(cartItems))
-                    window.localStorage.setItem("cart-items-number", Number(document.querySelector(".carrello").dataset.itemsNumber) + 1)
-                };
-            })
+            cartItems.push([valueName, valueTaglia, valueColor])
+            window.localStorage.setItem("cart", JSON.stringify(cartItems))
+            window.localStorage.setItem("cart-items-number", Number(document.querySelector(".carrello").dataset.itemsNumber) + 1)
+        })
 
-            document.querySelector(".carrello").dataset.itemsNumber++;
-        } 
+        document.querySelector(".carrello").dataset.itemsNumber++;
     }
 })
